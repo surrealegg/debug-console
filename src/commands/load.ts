17b38be0aa6c8c42759f13e030ce8884af0a3871
surrealegg <surrealegg@pm.me>
2022-04-17 22:30:59 +0400

    Fix load command crashing

--- src/commands/load.ts
+++ src/commands/load.ts
@@ -1,4 +1,9 @@
 import CommandHandler from "../handler";
+import { isValidInteger } from "../utils";
+
+declare let Galv: {
+    ASPLASH: { splashed: boolean };
+};
 
 const onCommand = (handler: CommandHandler, args: string[]) => {
     if (args.length < 2) {
@@ -8,26 +13,51 @@ const onCommand = (handler: CommandHandler, args: string[]) => {
 
     const id = parseInt(args[1]);
 
-    if (isNaN(id)) {
+    if (!isValidInteger(id)) {
         handler.log("Expected a number", "red");
         return;
     }
 
+    // Check whenever splash screen is played, because
+    // It will go back to the splash screen if it's not finished.
+    if (!Galv.ASPLASH.splashed) {
+        handler.log(
+            "Can not load a save file during splash screen.",
+            "red",
+        );
+        return;
+    }
+
+    // FIXME: This solution seems pretty hacky and it would be
+    // nice to find a better solution.
+    // The issue is that if the player is teleporting from Scene_Map
+    // it crashes because this.event() on YEP_SaveEventLocations is undefined.
+    // So the soultion is to move to an empty scene wait for a bit and teleport to
+    // a new scene.
     if (DataManager.loadGame(id)) {
         handler.setConsole(false);
+        SceneManager.goto(Scene_Base);
         SoundManager.playLoad();
-        if ($gameSystem.versionId() !== $dataSystem.versionId) {
-            $gamePlayer.reserveTransfer(
-                $gameMap.mapId(),
-                $gamePlayer.x,
-                $gamePlayer.y,
-                0,
-                0,
-            );
-            $gamePlayer.requestMapReload();
-        }
-        SceneManager.goto(Scene_Map);
-        handler.log(`Loaded gamefile ${id}`);
+        handler.log(`Loaded save file ${id}`);
+        setTimeout(() => {
+            if (DataManager.loadGame(id)) {
+                ConfigManager.save();
+                if (
+                    $gameSystem.versionId() !== $dataSystem.versionId
+                ) {
+                    $gamePlayer.reserveTransfer(
+                        $gameMap.mapId(),
+                        $gamePlayer.x,
+                        $gamePlayer.y,
+                        0,
+                        0,
+                    );
+                    $gamePlayer.requestMapReload();
+                }
+                $gameSystem.onAfterLoad();
+                SceneManager.push(Scene_Map);
+            }
+        }, 100);
         return;
     }
     handler.log(`Failed to load gamefile ${id}`, "red");
